2188d6e10184dccd65a77b9ef18ac02886168fb5
Mohammed Islam <mmislam101@gmail.com>
2019-11-30 22:10:48 -0800

    Move loading controls and rename AsyncActivity to BaseActivity

--- app/src/main/java/com/khatm/client/activities/AuthActivity.kt
+++ app/src/main/java/com/khatm/client/activities/AuthActivity.kt
@@ -6,22 +6,19 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.FrameLayout
-import android.widget.LinearLayout
-import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintSet
 import androidx.lifecycle.ViewModelProviders
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
 import com.khatm.client.R
-import com.khatm.client.extensions.AsyncActivity
+import com.khatm.client.extensions.BaseActivity
 import com.khatm.client.viewmodels.AuthViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
-class AuthActivity : AsyncActivity() {
+class AuthActivity : BaseActivity() {
 
     private lateinit var authViewModel: AuthViewModel
 
@@ -39,45 +36,31 @@ class AuthActivity : AsyncActivity() {
         googleSignInButton.setOnClickListener {
             signInGoogleAction()
         }
-
-        val layout: ConstraintLayout = findViewById(R.id.activity_auth);
-        layout.addView(createProgressView())
-    }
-
-    private fun createProgressView() : FrameLayout {
-        val inflater = LayoutInflater.from(this)
-        val layout = inflater.inflate(R.layout.view_progress, null, false) as FrameLayout
-        layout.setVisibility(View.VISIBLE);
-
-        var params = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT)
-
-        layout.layoutParams = params
-
-        return layout
     }
 
     private fun signInGoogleAction() {
-        GlobalScope.launch(Dispatchers.Main) {
-            val result = launchIntentAsync(authViewModel.signInIntent).await()
-
-            result?.data?.let {
-                try {
-                    val user = authViewModel.authorizeWithServerAsync(it).await()
-
-                    if (user?.access != null) {
-                        authViewModel.saveAuthorizedUserAsync(user).await()
-
-                        Log.d("AuthActivity", "Login successful")
-
-                        val intent = Intent(this@AuthActivity, HomeActivity::class.java)
-                        startActivity(intent)
-                    }
-                }
-                catch (e: ApiException) {
-                    Log.d("AuthActivity", "Failed: $e")
-                    Toast.makeText(this@AuthActivity, "Failed: $e", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
+        displayLoading()
+//        GlobalScope.launch(Dispatchers.Main) {
+//            val result = launchIntentAsync(authViewModel.signInIntent).await()
+//
+//            result?.data?.let {
+//                try {
+//                    val user = authViewModel.authorizeWithServerAsync(it).await()
+//
+//                    if (user?.access != null) {
+//                        authViewModel.saveAuthorizedUserAsync(user).await()
+//
+//                        Log.d("AuthActivity", "Login successful")
+//
+//                        val intent = Intent(this@AuthActivity, HomeActivity::class.java)
+//                        startActivity(intent)
+//                    }
+//                }
+//                catch (e: ApiException) {
+//                    Log.d("AuthActivity", "Failed: $e")
+//                    Toast.makeText(this@AuthActivity, "Failed: $e", Toast.LENGTH_SHORT).show()
+//                }
+//            }
+//        }
     }
 }
--- app/src/main/java/com/khatm/client/extensions/BaseActivity.kt
+++ app/src/main/java/com/khatm/client/extensions/BaseActivity.kt
@@ -1,29 +1,47 @@
 package com.khatm.client.extensions
 
 
-/*
-* Courtesy of: https://medium.com/@airtdave/streamlining-intents-with-kotlin-coroutines-33190a56c869
-* Used to create an async flow, a well as managing multiple intents
-*/
-
 import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.khatm.client.R
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.Deferred
 
 
-/**
- * Provides a mechanism to launch intents using co-routines.
- * Example usage:
- *
- * GlobalScope.launch(Dispatchers.Main) {
- *   val result = launchIntent(intent).await()
- *   result?.data?.let {
- *     // Do something with "it"
- *   }
- * }
- */
-abstract class AsyncActivity : AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity() {
+
+    /*
+    * Loading Indicator Controls
+    */
+
+    fun displayLoading() {
+        val layout: ConstraintLayout = findViewById(R.id.activity_auth);
+        layout.addView(loadingIndicatorView)
+    }
+
+    private val loadingIndicatorView : FrameLayout
+        get() {
+            val inflater = LayoutInflater.from(this)
+            val layout = inflater.inflate(R.layout.view_progress, null, false) as FrameLayout
+            layout.setVisibility(View.VISIBLE);
+
+            var params = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT)
+
+            layout.layoutParams = params
+
+            return layout
+        }
+
+    /*
+    * The following is code to observe responses to asynchronous Intent actions
+    *
+    * Courtesy of: https://medium.com/@airtdave/streamlining-intents-with-kotlin-coroutines-33190a56c869
+    * Used to create an async flow, a well as managing multiple intents
+    */
 
     var currentCode : Int = 0
     var resultByCode = mutableMapOf<Int, CompletableDeferred<ActivityResult?>>()
@@ -43,6 +61,15 @@ abstract class AsyncActivity : AppCompatActivity() {
      * @param intent the intent to be launched.
      *
      * @return Deferred<ActivityResult>
+
+     * Example usage:
+     *
+     * GlobalScope.launch(Dispatchers.Main) {
+     *   val result = launchIntentAsync(intent).await()
+     *   result?.data?.let {
+     *     // Do something with "it"
+     *   }
+     * }
      */
     fun launchIntentAsync(intent: Intent) : Deferred<ActivityResult?>
     {
