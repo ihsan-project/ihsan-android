5c76fb13a2696695fb6ebe9c1d7c2c1ef26fbb02
Mohammed Islam <mmislam101@gmail.com>
2020-04-12 20:23:41 -0700

    Add converter for Maps to store serialied map info

--- app/src/main/java/com/khatm/client/factories/DatabaseFactory.kt
+++ app/src/main/java/com/khatm/client/factories/DatabaseFactory.kt
@@ -1,16 +1,18 @@
 package com.khatm.client.factories
 
 import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
+import androidx.room.*
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.khatm.client.models.SettingsModel
 import com.khatm.client.models.UserModel
-import com.khatm.client.repositories.UserDao
 import com.khatm.client.repositories.SettingsDao
+import com.khatm.client.repositories.UserDao
+import java.lang.reflect.Type
 
 
 @Database(entities = [UserModel::class, SettingsModel::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class DatabaseFactory : RoomDatabase() {
 
     /* Configuration for DAOs */
@@ -38,5 +40,19 @@ abstract class DatabaseFactory : RoomDatabase() {
             }
             return INSTANCE
         }
+    }
+}
+
+class Converters {
+    @TypeConverter
+    fun fromString(value: String?): Map<String, Int> {
+        val mapType: Type = object : TypeToken<Map<String, Int>>() {}.getType()
+        return Gson().fromJson(value, mapType)
+    }
+
+    @TypeConverter
+    fun fromMap(map: Map<String, Int>): String {
+        val gson = Gson()
+        return gson.toJson(map)
     }
 }
