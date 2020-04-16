1a6b4e9b247c53340d678b65441c2899e2ca26e1
Mohammed Islam <mmislam101@gmail.com>
2020-04-15 22:56:20 -0700

    Add books model

--- app/src/main/java/com/khatm/client/models/BookModel.kt
+++ app/src/main/java/com/khatm/client/models/BookModel.kt
@@ -1,4 +1,16 @@
 package com.khatm.client.models
 
-class BookModel {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.squareup.moshi.Json
+
+data class Books(
+    val books: List<BookModel>
+)
+
+@Entity(tableName = "books")
+data class BookModel(
+    @PrimaryKey val version: Int,
+    val constants: Constants,
+    val features: Features
+)
