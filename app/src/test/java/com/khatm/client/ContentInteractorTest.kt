f5d142f867a414c6ad7db96eca7f905b52b766ad
Mohammed Islam <mmislam101@gmail.com>
2020-09-26 18:47:50 -0700

    Didn't need all that stuff

--- app/src/test/java/com/khatm/client/ContentInteractorTest.kt
+++ app/src/test/java/com/khatm/client/ContentInteractorTest.kt
@@ -6,35 +6,12 @@ import com.khatm.client.domain.models.Books
 import com.khatm.client.domain.repositories.BooksRepository
 
 import kotlinx.coroutines.*
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.setMain
 
-import org.junit.After
-import org.junit.Before
 import org.junit.Test
 import org.junit.Assert.*
 
 
 class ContentInteractorTest {
-    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
-
-    @Before
-    fun setup() {
-        // Sets the given [dispatcher] as an underlying dispatcher of [Dispatchers.Main].
-        // All consecutive usages of [Dispatchers.Main] will use given [dispatcher] under the hood.
-        Dispatchers.setMain(mainThreadSurrogate)
-    }
-
-    @After
-    fun tearDown() {
-        // Resets state of the [Dispatchers.Main] to the original main dispatcher.
-        // For example, in Android Main thread dispatcher will be set as [Dispatchers.Main].
-        Dispatchers.resetMain()
-
-        // Clean up the TestCoroutineDispatcher to make sure no other work is running.
-        mainThreadSurrogate.close()
-    }
-
     @Test
     fun syncBooks() {
         class BooksRepositoryInstance : BooksRepository {
@@ -66,6 +43,7 @@ class ContentInteractorTest {
             val books = contentInteractor.syncBooksAsync().await()
 
             assertEquals(1, books?.size)
+            assertEquals(1, books?.first()?.id)
         }
     }
 }
